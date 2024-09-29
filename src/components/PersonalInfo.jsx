59d20b9f2fb9d30f85ecf222d8a5e2df198e6844
J-Saisaran <saransai027@gmail.com>
2024-09-29 18:33:29 +0530

    adding icons

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -3,6 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchPersonalInfo } from '../redux/personalInfoSlice'; // Adjust the path as necessary
 import './PersonalInfo.css'; // Add CSS for styling
 
+// Import icons from MUI
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+
 const PersonalInfoComponent = () => {
   const dispatch = useDispatch();
   const { info, status, error } = useSelector((state) => state.personalInfo);
@@ -26,13 +31,17 @@ const PersonalInfoComponent = () => {
           <img src="Saisaran.jpg" alt="Profile" className="profile-photo" />
           <h1>{info.name}</h1>
           <p>{info.bio}</p>
-          <p>{info.email}</p>
           <p>
+            <EmailIcon /> {info.email}
+          </p>
+          <p>
+            <LinkedInIcon />{' '}
             <a href={info.linkedin} target="_blank" rel="noopener noreferrer">
               LinkedIn
             </a>
           </p>
           <p>
+            <GitHubIcon />{' '}
             <a href={info.github} target="_blank" rel="noopener noreferrer">
               GitHub
             </a>
